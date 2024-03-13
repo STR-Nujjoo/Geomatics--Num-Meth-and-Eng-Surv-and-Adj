75c2f41f90fdd7897b33e9a744c5c089bbb01c4d
your user github username <your github email>
2024-03-13 14:47:01 +0200

    Numerical Methods: R Solution to Problem 4

--- Solution to problem 4.R
+++ Solution to problem 4.R
@@ -22,10 +22,10 @@ hAo <- h1+df$deltah_1A
 hBo <- hAo+df$deltah_AB
 
 # Defining Design/Jacobian matrix with observation equations --------------
-J <- Deriv(~c(-dh1+dhA, # observation equation 1
-              -dhA+dhB, # observation equation 2
-              -dhB+dh2), # observation equation 3
-           c('dhA', 'dhB')) # jacobian w.r.t dhA and dhB
+J <- Deriv(~c(hAo-h1, # observation equation 1
+              hBo-hAo, # observation equation 2
+              h2-hBo), # observation equation 3
+           c('hAo', 'hBo')) # jacobian w.r.t hA and hB
 
 A <- matrix(J, 3, 2) # design matrix
 
@@ -34,14 +34,14 @@ weights <- c(1/(df$sd_1A)^2, 1/(df$sd_AB)^2, 1/(df$sd_B2)^2) # turning standard 
 P <- diag(weights) # weight matrixs
 
 # Defining misclosure matrix ----------------------------------------------
-misclosure1 <- expression(dh1A-(hAo-h1)) # misclosure equation 1
-misclosure2 <- expression(dhAB-(hBo-hAo)) # misclosure equation 2
-misclosure3 <- expression(dhB2-(h2-hBo)) # misclosure equation 3
+misclosure1 <- expression(dh1-(hAo-h1)) # misclosure equation 1
+misclosure2 <- expression(dh2-(hBo-hAo)) # misclosure equation 2
+misclosure3 <- expression(dh3-(h2-hBo)) # misclosure equation 3
 
 # defining terms
-dh1A <- df$deltah_1A 
-dhAB <- df$deltah_AB
-dhB2 <- df$deltah_B2
+dh1 <- df$deltah_1A 
+dh2 <- df$deltah_AB
+dh3 <- df$deltah_B2
 
 # evaluating misclosures
 l1 <- eval(misclosure1) 
@@ -58,42 +58,3 @@ hA_adj <- hAo + x[1,] # adjusted height at A
 hB_adj <- hBo + x[2,] # adjusted height at B
 
 # The end -----------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
